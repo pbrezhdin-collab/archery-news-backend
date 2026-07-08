0a90605586780a15a597a59e556b9f41857fe885
pbrezhdin-collab <p.brezhdin@gmail.com>
2026-07-08 20:10:53 +0300

    Delete app directory

--- app/routers/categories.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.schemas import CATEGORIES
-
-router = APIRouter(prefix="/api/categories", tags=["categories"])
-
-
-@router.get("")
-async def get_categories() -> list[str]:
-    return CATEGORIES
